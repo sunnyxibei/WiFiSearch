00cfc43fbbfa57e216a1f6130e025bfae9b7d1e5
Yuanbin <jiayuanbin@gmail.com>
2020-06-02 17:17:41 +0800

    Init project

---
+++ app/src/main/java/com/example/wifisearch/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.wifisearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 扫描并显示周边Wi-Fi列表
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/wifisearch/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.wifisearch
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+
+
+}
